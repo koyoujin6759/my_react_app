af12068ba7b508066c65af426e02c545fbb0d968
koyoujin6759 <rhdbwls853@naver.com>
2024-12-02 20:36:10 +0900

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  //자식컴포넌트
+  //header 함수 component
+  //const header() => {}
+  //첫글자는 대문자
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
+export default Header; //header 컴포넌트를 기본으로 내보냄.
