ffd50b008a14da3000f8cd7315a0dbb31959e520
koyoujin6759 <rhdbwls853@naver.com>
2024-12-08 16:58:21 +0900

    241208

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import Register from "./components/Register";
 import HookExam from "./components/HookExam";
 import Register2 from "./components/Register2";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClickButton = (value) => {
+    setCount(count + value);
+    console.log("count", count); // state함수는 비동기이기때문에 setcount함수실행 이전의값을 콘솔에찍는다.
+  };
+
+  useEffect(() => {
+    console.log(`${count}`); //변경된 state값을 바로 출력함.
+  }, [count]); //의존성 배열, deps
+
   return (
-    <>
-      <Register2 />
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        {/* viewer컴포넌트에 Count props 전달 */}
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+
+      {/* <Register2 /> */}
       {/* <HookExam /> */}
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Viewer.jsx
@@ -0,0 +1,9 @@
+const Viewer = ({ count }) => {
+  return (
+    <div>
+      <div>현재 카운트: </div>
+      <div>{count}</div>
+    </div>
+  );
+};
+export default Viewer;
